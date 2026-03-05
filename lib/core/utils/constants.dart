7240fb6c49a82746ece04045a8b0526eab72303b
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-05 16:33:48 +0200

    change api key

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   static const String baseUrl = "https://api.weatherapi.com/v1/";
 
-  static const String apiKey = "c503eee9fe83d92836d84b1edf6a31ab";
+  static const String apiKey = "14084af3cd4642f58d8143105260503";
 }
