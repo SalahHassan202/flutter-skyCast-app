19a5af68c519faa53110f61544a1c8d840954db1
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-05 16:25:30 +0200

    added colors file

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff4A90E2);
+
+  static const Color background = Color(0xff1B1D2A);
+
+  static const Color card = Color(0xff2E335A);
+
+  static const Color white = Colors.white;
+
+  static const Color grey = Colors.grey;
+}
